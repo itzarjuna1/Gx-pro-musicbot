9e587a1e2074125bab47eacc201b6037e980dec7
ARJUN <pachangedinanath@gmail.com>
2025-08-02 23:51:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,10 +6,10 @@ from pyrogram import filters
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID", "21883290"))
-API_HASH = getenv("API_HASH", "d58e9d672b9cbbe8d6c3d3fa0d0546e9")
-BOT_TOKEN = getenv("BOT_TOKEN", "7399017823:AAHMF85X6WeCcr_JY2A_IARgXfL80laD7qI")
-MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://knight4563:knight4563@cluster0.a5br0se.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+API_ID = int(getenv("API_ID", ""))
+API_HASH = getenv("API_HASH", "")
+BOT_TOKEN = getenv("BOT_TOKEN", "")
+MONGO_DB_URI = getenv("MONGO_DB_URI", "")
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 1000000))
 LOGGER_ID = int(getenv("LOGGER_ID", "-1002823976818"))
 OWNER_ID = int(getenv("OWNER_ID", "8473985518"))
@@ -17,7 +17,7 @@ OWNER_ID = int(getenv("OWNER_ID", "8473985518"))
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/itzarjuna1/Gx-pro-musicbot")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
@@ -34,7 +34,7 @@ PLAYLIST_FETCH_LIMIT = int(getenv("PLAYLIST_FETCH_LIMIT", 25))
 TG_AUDIO_FILESIZE_LIMIT = int(getenv("TG_AUDIO_FILESIZE_LIMIT", 104857600))
 TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", 1073741824))
 
-STRING1 = getenv("STRING_SESSION", "AQFN6ZoALLm-WfXZxh-rFTu5Ow-kyHuONC_UhozKXDXoE-1SXqrpsCOeL02oKDA45gwXJr_j7l0M5GgLuDcZcum743XLB332D1JBQgmQZ2JjX4FstCl-E7iDh62rawrAOrzoZJsg3X_1vjXE6JWkwXuKRvV0_COk2_6kqdE4V0liPryPxH9yMyNrDwPcOPmzQvJZkZmwvUcWtG_FXnCykEQEN9Se-kG5SVbeRZ_b--DRAffNVoyfB-e2bRfuRMqKVnH9UnScXP0TMVBz_2QoteOosaQsjWvgU7gjBnKdvvdhEvoHyeOHtd1xts2vy8eTDmCvu8u-dKWFJdT51xad2q9yyGM0AQAAAAHRpZTBAA")
+STRING1 = getenv("STRING_SESSION", "")
 STRING2 = getenv("STRING_SESSION2", None)
 STRING3 = getenv("STRING_SESSION3", None)
 STRING4 = getenv("STRING_SESSION4", None)
