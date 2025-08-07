b22ff0fb3a0793a5e1f438a6c86c8ce3fded9238
ARJUN <pachangedinanath@gmail.com>
2025-08-07 10:15:29 +0530

    Update call.py

--- EsproMusic/core/call.py
+++ EsproMusic/core/call.py
@@ -7,9 +7,10 @@ from pyrogram import Client
 from pyrogram.types import InlineKeyboardMarkup
 from pytgcalls import PyTgCalls, StreamType
 from pytgcalls.exceptions import (
-    AlreadyJoinedError,
     NoActiveGroupCall,
-    TelegramServerError,
+    AlreadyJoinedError,
+    NotInGroupCallError,
+    GroupCallNotFoundError,
 )
 from pytgcalls.types import Update
 from pytgcalls.types.input_stream import AudioPiped, AudioVideoPiped
