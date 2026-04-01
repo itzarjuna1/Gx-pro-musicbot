8edd48cf7fca908a74891f7127ccd8fd45addc0a
ARJUN <pachangedinanath@gmail.com>
2026-04-01 12:53:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,13 +6,13 @@ from pyrogram import filters
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID", "35279715"))
-API_HASH = getenv("API_HASH", "b4c339216397b5941d88c8617d2dc12b")
+API_ID = int(getenv("API_ID", "39679517"))
+API_HASH = getenv("API_HASH", "aed61e5ff8c711895f8b0c99e51c16cc")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
 MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://knight4563:knight4563@cluster0.a5br0se.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 1000000))
-LOGGER_ID = int(getenv("LOGGER_ID", "-1003468243393"))
-OWNER_ID = int(getenv("OWNER_ID", "7651303468"))
+LOGGER_ID = int(getenv("LOGGER_ID", "-1003882647583"))
+OWNER_ID = int(getenv("OWNER_ID", "8364692780"))
 
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
@@ -21,8 +21,8 @@ UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/itzarjuna1/Gx-pro-mu
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/dark_musictm")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/snowy_support")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/theinfinitynetwork")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/theinfinity_suppport")
 
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", false))
 
@@ -47,18 +47,18 @@ votemode = {}
 autoclean = []
 confirmer = {}
 
-START_IMG_URL = getenv("START_IMG_URL", "https://files.catbox.moe/8v3sw6.jpg")
-PING_IMG_URL = getenv("PING_IMG_URL", "https://files.catbox.moe/8v3sw6.jpg")
-PLAYLIST_IMG_URL = "https://files.catbox.moe/8v3sw6.jpg"
-STATS_IMG_URL = "https://files.catbox.moe/8v3sw6.jpg"
-TELEGRAM_AUDIO_URL = "https://files.catbox.moe/8v3sw6.jpg"
-TELEGRAM_VIDEO_URL = "https://files.catbox.moe/8v3sw6.jpg"
-STREAM_IMG_URL = "https://files.catbox.moe/8v3sw6.jpg"
-SOUNCLOUD_IMG_URL = "https://files.catbox.moe/8v3sw6.jpg"
-YOUTUBE_IMG_URL = "https://files.catbox.moe/8v3sw6.jpg"
-SPOTIFY_ARTIST_IMG_URL = "https://files.catbox.moe/8v3sw6.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://files.catbox.moe/8v3sw6.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://files.catbox.moe/8v3sw6.jpg"
+START_IMG_URL = getenv("START_IMG_URL", "https://graph.org/file/871fc617654eb6b013a8b-dad2b73a5bdc79a3f2.jpg")
+PING_IMG_URL = getenv("PING_IMG_URL", "https://graph.org/file/871fc617654eb6b013a8b-dad2b73a5bdc79a3f2.jpg")
+PLAYLIST_IMG_URL = "https://graph.org/file/871fc617654eb6b013a8b-dad2b73a5bdc79a3f2.jpg"
+STATS_IMG_URL = "https://graph.org/file/871fc617654eb6b013a8b-dad2b73a5bdc79a3f2.jpg"
+TELEGRAM_AUDIO_URL = "https://graph.org/file/871fc617654eb6b013a8b-dad2b73a5bdc79a3f2.jpg"
+TELEGRAM_VIDEO_URL = "https://graph.org/file/871fc617654eb6b013a8b-dad2b73a5bdc79a3f2.jpg"
+STREAM_IMG_URL = "https://graph.org/file/871fc617654eb6b013a8b-dad2b73a5bdc79a3f2.jpg"
+SOUNCLOUD_IMG_URL = "https://graph.org/file/871fc617654eb6b013a8b-dad2b73a5bdc79a3f2.jpg"
+YOUTUBE_IMG_URL = "https://graph.org/file/871fc617654eb6b013a8b-dad2b73a5bdc79a3f2.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://graph.org/file/871fc617654eb6b013a8b-dad2b73a5bdc79a3f2.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://graph.org/file/871fc617654eb6b013a8b-dad2b73a5bdc79a3f2.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = "https://graph.org/file/871fc617654eb6b013a8b-dad2b73a5bdc79a3f2.jpg"
 
 def time_to_seconds(time):
     stringt = str(time)
