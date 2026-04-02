e26a465e6fc78484c23a84c6415378b40d78005e
ARJUN <pachangedinanath@gmail.com>
2026-04-02 09:35:26 +0530

    Add custom emoji IDs to inline buttons

--- EsproMusic/utils/inline/start.py
+++ EsproMusic/utils/inline/start.py
@@ -10,7 +10,7 @@ def start_panel(_):
             InlineKeyboardButton(
                 text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
             ),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT, icon_custom_emoji_id=5247029067256987229),
         ],
     ]
     return buttons
@@ -19,20 +19,18 @@ def start_panel(_):
 def private_panel(_):
     buttons = [
         [
-            InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper"),
+            InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper", icon_custom_emoji_id=5818705028424141605),
         ],
         [
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
-            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
+            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT, icon_custom_emoji_id=5971867376130461576),
+            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL, icon_custom_emoji_id=5960842268096073715),
         ],
         [
             InlineKeyboardButton(
                 text=_["S_B_3"],
                 url=f"https://t.me/{app.username}?startgroup=true",
+                icon_custom_emoji_id=6312260233171312151
             )
-        ],
-        [
-            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
         ],
     ]
     return buttons
