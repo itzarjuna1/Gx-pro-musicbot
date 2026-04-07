7d1637a0ad818a31bbe676f487c0758d482aa955
uppermooninfinity <uppermooninfinity@gmail.com>
2026-04-07 12:42:29 +0530

    𝐄𝗌ρ𝗋ⱺ 𝐌ᥙsiᥴ [ ɴᴏ ᴀᴅʂ ] 🕊

--- EsproMusic/plugins/tools/harem.py
+++ EsproMusic/plugins/tools/harem.py
@@ -8,7 +8,7 @@ import html
 import random
 from collections import Counter
 from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler, filters
-from EsproMusic import db, collection, user_totals_collection, user_collection, top_global_groups_collection, top_global_groups_collection, group_user_totals_collection, application
+from config import db, collection, user_totals_collection, user_collection, top_global_groups_collection, top_global_groups_collection, group_user_totals_collection, application
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, InlineQueryResultPhoto, InputTextMessageContent, InputMediaPhoto
 from telegram import InlineQueryResultArticle, InputTextMessageContent, InlineKeyboardMarkup, InlineKeyboardButton
 import asyncio
