10a6b830fe9ca6ffac02f4f84a6bd66182def5f2
uppermooninfinity <uppermooninfinity@gmail.com>
2026-04-07 12:41:43 +0530

    𝐄𝗌ρ𝗋ⱺ 𝐌ᥙsiᥴ [ ɴᴏ ᴀᴅʂ ] 🕊

--- EsproMusic/plugins/tools/changetime.py
+++ EsproMusic/plugins/tools/changetime.py
@@ -2,8 +2,8 @@ from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler, filters
 from pymongo import MongoClient, ReturnDocument
 from motor.motor_asyncio import AsyncIOMotorClient 
-from EsproMusic import application 
-from EsproMusic import db, collection, user_totals_collection, user_collection, top_global_groups_collection, top_global_groups_collection, group_user_totals_collection
+from config import application 
+from config import db, collection, user_totals_collection, user_collection, top_global_groups_collection, top_global_groups_collection, group_user_totals_collection
 #waifu added vy itzarjuna1
 async def change_time(update: Update, context: CallbackContext) -> None:
     
