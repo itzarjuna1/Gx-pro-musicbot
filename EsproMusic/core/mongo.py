329882c357c9df2a4fe4afb174cf94ca27771cf6
˹ 🎧 ᴛᴇᴀᴍ ɪɴғɪɴɪᴛʏ ʙᴏᴛs ˼ <networksmite@gmail.com>
2026-04-09 05:27:37 +0530

    𝐄𝗌ρ𝗋ⱺ 𝐌ᥙsiᥴ [ ɴᴏ ᴀᴅʂ ] 🕊

--- EsproMusic/core/mongo.py
+++ EsproMusic/core/mongo.py
@@ -1,10 +1,9 @@
 from motor.motor_asyncio import AsyncIOMotorClient
-
 from config import MONGO_DB_URI
-
 from ..logging import LOGGER
 
 LOGGER(__name__).info("Connecting to your Mongo Database...")
+
 try:
     _mongo_async_ = AsyncIOMotorClient(MONGO_DB_URI)
     mongodb = _mongo_async_.Anon
@@ -12,3 +11,12 @@ try:
 except:
     LOGGER(__name__).error("Failed to connect to your Mongo Database.")
     exit()
+
+# ================= COLLECTIONS =================
+
+groups = mongodb.groups     # for nsfw toggle per group
+NSFW = mongodb.nsfw         # for scanned media cache
+
+# ================= STORAGE =================
+
+NSFW_STORAGE = -1003882647583 # <-- replace with your channel id
