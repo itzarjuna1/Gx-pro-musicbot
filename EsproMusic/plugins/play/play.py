21abebe3f59e44319001c9079b79bae98226d23c
uppermooninfinity <uppermooninfinity@gmail.com>
2026-04-10 12:30:08 +0530

    𝐄𝗌ρ𝗋ⱺ 𝐌ᥙsiᥴ [ ɴᴏ ᴀᴅʂ ] 🕊

--- EsproMusic/plugins/play/play.py
+++ EsproMusic/plugins/play/play.py
@@ -97,6 +97,10 @@ async def play_commnd(
             }
 
             try:
+                if isinstance(details, dict) and 
+            details.get("source") == "jiosaavn":
+                    details["path"] = details["link"]
+                
                 await stream(
                     _,
                     mystic,
@@ -675,13 +679,3 @@ async def slider_queries(client, CallbackQuery, _):
         )
 
 
-# ©️ Copyright Reserved - @NoxxOP  Nand Yaduwanshi
-
-# ===========================================
-# ©️ 2025 Nand Yaduwanshi (aka @NoxxOP)
-# 🔗 GitHub : https://github.com/NoxxOP/ShrutiMusic
-# 📢 Telegram Channel : https://t.me/ShrutiBots
-# ===========================================
-
-
-# ❤️ Love From ShrutiBots 
