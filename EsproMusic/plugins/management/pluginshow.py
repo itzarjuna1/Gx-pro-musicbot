973a83a7b90b4a7219749bdf7a77e4110dd29fd0
uppermooninfinity <uppermooninfinity@gmail.com>
2026-04-11 22:38:24 +0530

    𝐄𝗌ρ𝗋ⱺ 𝐌ᥙsiᥴ [ ɴᴏ ᴀᴅʂ ] 🕊

--- EsproMusic/plugins/management/pluginshow.py
+++ EsproMusic/plugins/management/pluginshow.py
@@ -1,3 +1,5 @@
+#file written by @itzarjuna01 © some errors spotted were fixed via ai 
+#any marks of ai should be considered as ai fixes 
 # ================== PLUGIN SHOW SYSTEM ==================
 
 import os
