2907b6acac99d0010c17cdce2185af5548fb7525
uppermooninfinity <uppermooninfinity@gmail.com>
2026-04-12 20:15:32 +0530

    𝐄𝗌ρ𝗋ⱺ 𝐌ᥙsiᥴ [ ɴᴏ ᴀᴅʂ ] 🕊

--- EsproMusic/plugins/tools/trade.py
+++ EsproMusic/plugins/tools/trade.py
@@ -1,215 +1,183 @@
-from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from motor.motor_asyncio import AsyncIOMotorClient 
-from pyrogram import Client, filters
-from pyrogram.types import InlineQueryResultPhoto, InputTextMessageContent
-from collections import Counter
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
-from pyrogram import enums
-from config import db, collection, top_global_groups_collection, group_user_totals_collection, user_collection, user_totals_collection
-from EsproMusic import Loy
+from pyrogram import filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 
-#final fixes done by me
+from EsproMusic import Loy
+from config import user_collection
 
 
 pending_trades = {}
-
-
-@Loy.on_message(filters.command("trade"))
-async def trade(client, message):
-    sender_id = message.from_user.id
-
-    if not message.reply_to_message:
-        await message.reply_text("❖ ʏᴏᴜ ɴᴇᴇᴅ ᴛᴏ ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴜsᴇʀ's ᴍᴇssᴀɢᴇ ᴛᴏ ᴛʀᴀᴅᴇ ᴀ ᴄʜᴀʀᴀᴄᴛᴇʀ.")
-        return
-
-    receiver_id = message.reply_to_message.from_user.id
-
-    if sender_id == receiver_id:
-        await message.reply_text("❖ ʏᴏᴜ ᴄᴀɴ'ᴛ ᴛʀᴀᴅᴇ ᴀ ᴄʜᴀʀᴀᴄᴛᴇʀ ᴡɪᴛʜ ʏᴏᴜʀsᴇʟғ.")
-        return
-
-    if len(message.command) != 3:
-        await message.reply_text("❖ ʏᴏᴜ ɴᴇᴇᴅ ᴛᴏ ᴘʀᴏᴠɪᴅᴇ ᴛᴡᴏ ᴄʜᴀʀᴀᴄᴛᴇʀ ɪᴅ,s.")
-        return
-
-    sender_character_id, receiver_character_id = message.command[1], message.command[2]
-
-    sender = await user_collection.find_one({'id': sender_id})
-    receiver = await user_collection.find_one({'id': receiver_id})
-
-    sender_character = next((character for character in sender['characters'] if character['id'] == sender_character_id), None)
-    receiver_character = next((character for character in receiver['characters'] if character['id'] == receiver_character_id), None)
-
-    if not sender_character:
-        await message.reply_text("❖ ʏᴏᴜ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴛʜᴇ ᴄʜᴀʀᴀᴄᴛᴇʀ ʏᴏᴜ'ʀᴇ ᴛʀʏɪɴɢ ᴛᴏ ᴛʀᴀᴅᴇ.")
-        return
-
-    if not receiver_character:
-        await message.reply_text("❖ ᴛʜᴇ ᴏᴛʜᴇʀ ᴜsᴇʀ ᴅᴏᴇsɴ'ᴛ ʜᴀᴠᴇ ᴛʜᴇ ᴄʜᴀʀᴀᴄᴛᴇʀ, ᴛʜᴇʏ ᴀʀᴇ ᴛʀʏɪɴɢ ᴛᴏ ᴛʀᴀᴅᴇ.")
-        return
-
-    # Rest of your code...
-
-
-
-
-    if len(message.command) != 3:
-        await message.reply_text("❖  ➥ /trade [ʏᴏᴜʀ ᴄʜᴀʀᴀᴄᴛᴇʀ ɪᴅ] [ᴏᴛʜᴇʀ ᴜsᴇʀ ᴄʜᴀʀᴀᴄᴛᴇʀ ɪᴅ]")
-        return
-
-    sender_character_id, receiver_character_id = message.command[1], message.command[2]
-
-    # Add the trade offer to the pending trades
-    pending_trades[(sender_id, receiver_id)] = (sender_character_id, receiver_character_id)
-
-    # Create a confirmation button
-    keyboard = InlineKeyboardMarkup(
-        [
-            [InlineKeyboardButton("ᴄᴏɴғɪʀᴍ ᴛʀᴀᴅᴇ", callback_data="confirm_trade")],
-            [InlineKeyboardButton("ᴄᴀɴᴄᴇʟ ᴛʀᴀᴅᴇ", callback_data="cancel_trade")]
-        ]
-    )
-
-    await message.reply_text(f"❖ {message.reply_to_message.from_user.mention}, ᴅᴏ ʏᴏᴜ ᴀᴄᴄᴇᴘᴛ ᴛʜɪs ᴛʀᴀᴅᴇ ?", reply_markup=keyboard)
-
-
-@Loy.on_callback_query(filters.create(lambda _, __, query: query.data in ["confirm_trade", "cancel_trade"]))
-async def on_callback_query(client, callback_query):
-    receiver_id = callback_query.from_user.id
-
-    # Find the trade offer
-    for (sender_id, _receiver_id), (sender_character_id, receiver_character_id) in pending_trades.items():
-        if _receiver_id == receiver_id:
-            break
-    else:
-        await callback_query.answer("❖ ᴛʜɪs ɪs ɴᴏᴛ ғᴏʀ ʏᴏᴜ.", show_alert=True)
-        return
-
-    if callback_query.data == "confirm_trade":
-        # Perform the trade
-        sender = await user_collection.find_one({'id': sender_id})
-        receiver = await user_collection.find_one({'id': receiver_id})
-
-        sender_character = next((character for character in sender['characters'] if character['id'] == sender_character_id), None)
-        receiver_character = next((character for character in receiver['characters'] if character['id'] == receiver_character_id), None)
-
-        # Remove the characters from the users' collections
-        sender['characters'].remove(sender_character)
-        receiver['characters'].remove(receiver_character)
-
-        # Update the users' collections in the database
-        await user_collection.update_one({'id': sender_id}, {'$set': {'characters': sender['characters']}})
-        await user_collection.update_one({'id': receiver_id}, {'$set': {'characters': receiver['characters']}})
-
-        # Add the characters to the other users' collections
-        sender['characters'].append(receiver_character)
-        receiver['characters'].append(sender_character)
-
-        # Update the users' collections in the database again
-        await user_collection.update_one({'id': sender_id}, {'$set': {'characters': sender['characters']}})
-        await user_collection.update_one({'id': receiver_id}, {'$set': {'characters': receiver['characters']}})
-
-        # Remove the trade offer from the pending trades
-        del pending_trades[(sender_id, receiver_id)]
-
-        await callback_query.message.edit_text(f"❖ ʏᴏᴜ ʜᴀᴠᴇ sᴜᴄᴄᴇssғᴜʟʟʏ ᴛʀᴀᴅᴇ ʏᴏᴜʀ ᴄʜᴀʀᴀᴄᴛᴇʀ ᴡɪᴛʜ ➥ {callback_query.message.reply_to_message.from_user.mention} !")
-
-    elif callback_query.data == "cancel_trade":
-        # Remove the trade offer from the pending trades
-        del pending_trades[(sender_id, receiver_id)]
-
-        await callback_query.message.edit_text("❌️ ᴄᴀɴᴄᴇʟʟᴇᴅ...")
-
-
-
-# This dictionary will hold the gift offers until they are confirmed or cancelled
 pending_gifts = {}
 
 
-@Loy.on_message(filters.command("gift"))
-async def gift(client, message):
-    sender_id = message.from_user.id
+def get_char(user, cid):
+    return next((c for c in user.get("characters", []) if c.get("id") == cid), None)
+
+
+# ================= TRADE =================
+
+@Loy.on_message(filters.command("trade") & filters.group)
+async def trade(_, message):
 
     if not message.reply_to_message:
-        await message.reply_text("❖ ʏᴏᴜ ɴᴇᴇᴅ ᴛᴏ ʀᴇᴘʟʏ ᴀ ᴜsᴇʀs's ᴍᴇssᴀɢᴇ ᴛᴏ ɢɪғᴛ ᴀ ᴄʜᴀʀᴀᴄᴛᴇʀ.")
-        return
+        return await message.reply("❖ ʀᴇᴘʟʏ ᴛᴏ ᴜsᴇʀ")
 
+    if len(message.command) != 3:
+        return await message.reply("❖ /trade your_id other_id")
+
+    sender_id = message.from_user.id
     receiver_id = message.reply_to_message.from_user.id
-    receiver_username = message.reply_to_message.from_user.username
-    receiver_first_name = message.reply_to_message.from_user.first_name
 
     if sender_id == receiver_id:
-        await message.reply_text("❖ ʏᴏᴜ ᴄᴀɴ'ᴛ ɢɪғᴛ ᴀ ᴄʜᴀʀᴀᴄᴛᴇʀ ᴛᴏ ʏᴏᴜʀsᴇʟғ.")
-        return
+        return await message.reply("❖ ᴄᴀɴᴛ ᴛʀᴀᴅᴇ ʏᴏᴜʀsᴇʟғ")
 
-    if len(message.command) != 2:
-        await message.reply_text("❖ ʏᴏᴜ ɴᴇᴇᴅ ᴛᴏ ᴘʀᴏᴠɪᴅᴇ ᴀ ᴄʜᴀʀᴀᴄᴛᴇʀ ɪᴅ.")
-        return
+    s_id, r_id = message.command[1], message.command[2]
 
-    character_id = message.command[1]
+    sender = await user_collection.find_one({"id": sender_id})
+    receiver = await user_collection.find_one({"id": receiver_id})
 
-    sender = await user_collection.find_one({'id': sender_id})
+    if not sender or not receiver:
+        return await message.reply("❖ ᴜsᴇʀ ᴅᴀᴛᴀ ɴᴏᴛ ғᴏᴜɴᴅ")
 
-    character = next((character for character in sender['characters'] if character['id'] == character_id), None)
+    sc = get_char(sender, s_id)
+    rc = get_char(receiver, r_id)
 
-    if not character:
-        await message.reply_text("❖ ʏᴏᴜ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴛʜɪs ᴄʜᴀʀᴀᴄᴛᴇʀ ɪɴ ʏᴏᴜʀ ᴄᴏʟʟᴇᴄᴛɪᴏɴ.")
-        return
+    if not sc:
+        return await message.reply("❖ ʏᴏᴜ ᴅᴏɴᴛ ʜᴀᴠᴇ ᴛʜɪs")
 
-    # Add the gift offer to the pending gifts
-    pending_gifts[(sender_id, receiver_id)] = {
-        'character': character,
-        'receiver_username': receiver_username,
-        'receiver_first_name': receiver_first_name
-    }
+    if not rc:
+        return await message.reply("❖ ᴛʜᴇʏ ᴅᴏɴᴛ ʜᴀᴠᴇ ᴛʜɪs")
 
-    # Create a confirmation button
-    keyboard = InlineKeyboardMarkup(
-        [
-            [InlineKeyboardButton("ᴄᴏɴғɪʀᴍ ɢɪғᴛ", callback_data="confirm_gift")],
-            [InlineKeyboardButton("ᴄᴀɴᴄᴇʟ ɢɪғᴛ", callback_data="cancel_gift")]
-        ]
+    pending_trades[(sender_id, receiver_id)] = (s_id, r_id)
+
+    kb = InlineKeyboardMarkup([
+        [InlineKeyboardButton("✅ ʏᴇs", callback_data=f"trade_yes|{sender_id}")],
+        [InlineKeyboardButton("❌ ɴᴏ", callback_data=f"trade_no|{sender_id}")]
+    ])
+
+    await message.reply(
+        f"❖ {message.reply_to_message.from_user.mention} ᴀᴄᴄᴇᴘᴛ ᴛʀᴀᴅᴇ?",
+        reply_markup=kb
     )
 
-    await message.reply_text(f"❖ ᴅᴏ ʏᴏᴜ ʀᴇᴀʟʟʏ ᴡᴀɴɴᴀ ᴛᴏ ɢɪғᴛ ➥ {message.reply_to_message.from_user.mention}?", reply_markup=keyboard)
 
-@Loy.on_callback_query(filters.create(lambda _, __, query: query.data in ["confirm_gift", "cancel_gift"]))
-async def on_callback_query(client, callback_query):
-    sender_id = callback_query.from_user.id
+@Loy.on_callback_query(filters.regex("^trade_"))
+async def trade_cb(_, q: CallbackQuery):
 
-    # Find the gift offer
-    for (_sender_id, receiver_id), gift in pending_gifts.items():
-        if _sender_id == sender_id:
-            break
+    data = q.data.split("|")
+    action = data[0]
+    sender_id = int(data[1])
+    receiver_id = q.from_user.id
+
+    key = (sender_id, receiver_id)
+
+    if key not in pending_trades:
+        return await q.answer("ɴᴏ ᴛʀᴀᴅᴇ", True)
+
+    if action == "trade_no":
+        pending_trades.pop(key, None)
+        return await q.message.edit("❌ ᴄᴀɴᴄᴇʟʟᴇᴅ")
+
+    s_id, r_id = pending_trades[key]
+
+    sender = await user_collection.find_one({"id": sender_id})
+    receiver = await user_collection.find_one({"id": receiver_id})
+
+    sc = get_char(sender, s_id)
+    rc = get_char(receiver, r_id)
+
+    if not sc or not rc:
+        return await q.message.edit("❌ ᴇʀʀᴏʀ")
+
+    sender["characters"].remove(sc)
+    receiver["characters"].remove(rc)
+
+    sender["characters"].append(rc)
+    receiver["characters"].append(sc)
+
+    await user_collection.update_one({"id": sender_id}, {"$set": {"characters": sender["characters"]}})
+    await user_collection.update_one({"id": receiver_id}, {"$set": {"characters": receiver["characters"]}})
+
+    pending_trades.pop(key, None)
+
+    await q.message.edit("✅ ᴛʀᴀᴅᴇ sᴜᴄᴄᴇss")
+
+
+# ================= GIFT =================
+
+@Loy.on_message(filters.command("gift") & filters.group)
+async def gift(_, message):
+
+    if not message.reply_to_message:
+        return await message.reply("❖ ʀᴇᴘʟʏ ᴛᴏ ᴜsᴇʀ")
+
+    if len(message.command) != 2:
+        return await message.reply("❖ /gift char_id")
+
+    sender_id = message.from_user.id
+    receiver_id = message.reply_to_message.from_user.id
+
+    if sender_id == receiver_id:
+        return await message.reply("❖ ɴᴏ sᴇʟғ")
+
+    cid = message.command[1]
+
+    sender = await user_collection.find_one({"id": sender_id})
+
+    if not sender:
+        return await message.reply("❖ ɴᴏ ᴅᴀᴛᴀ")
+
+    char = get_char(sender, cid)
+
+    if not char:
+        return await message.reply("❖ ɴᴏᴛ ғᴏᴜɴᴅ")
+
+    pending_gifts[(sender_id, receiver_id)] = char
+
+    kb = InlineKeyboardMarkup([
+        [InlineKeyboardButton("✅ ʏᴇs", callback_data=f"gift_yes|{receiver_id}")],
+        [InlineKeyboardButton("❌ ɴᴏ", callback_data=f"gift_no|{receiver_id}")]
+    ])
+
+    await message.reply(
+        f"❖ ᴄᴏɴғɪʀᴍ ɢɪғᴛ ᴛᴏ {message.reply_to_message.from_user.mention}?",
+        reply_markup=kb
+    )
+
+
+@Loy.on_callback_query(filters.regex("^gift_"))
+async def gift_cb(_, q: CallbackQuery):
+
+    data = q.data.split("|")
+    action = data[0]
+    receiver_id = int(data[1])
+    sender_id = q.from_user.id
+
+    key = (sender_id, receiver_id)
+
+    if key not in pending_gifts:
+        return await q.answer("ɴᴏ ɢɪғᴛ", True)
+
+    if action == "gift_no":
+        pending_gifts.pop(key, None)
+        return await q.message.edit("❌ ᴄᴀɴᴄᴇʟʟᴇᴅ")
+
+    char = pending_gifts[key]
+
+    sender = await user_collection.find_one({"id": sender_id})
+    receiver = await user_collection.find_one({"id": receiver_id})
+
+    sender["characters"].remove(char)
+
+    await user_collection.update_one({"id": sender_id}, {"$set": {"characters": sender["characters"]}})
+
+    if receiver:
+        await user_collection.update_one({"id": receiver_id}, {"$push": {"characters": char}})
     else:
-        await callback_query.answer("❖ ᴛʜɪs ɪs ɴᴏᴛ ғᴏʀ ʏᴏᴜ.", show_alert=True)
-        return
+        await user_collection.insert_one({
+            "id": receiver_id,
+            "characters": [char]
+        })
 
-    if callback_query.data == "confirm_gift":
-        # Perform the gift
-        sender = await user_collection.find_one({'id': sender_id})
-        receiver = await user_collection.find_one({'id': receiver_id})
+    pending_gifts.pop(key, None)
 
-        # Remove the character from the sender's collection
-        sender['characters'].remove(gift['character'])
-        await user_collection.update_one({'id': sender_id}, {'$set': {'characters': sender['characters']}})
-
-        # Add the character to the receiver's collection
-        if receiver:
-            await user_collection.update_one({'id': receiver_id}, {'$push': {'characters': gift['character']}})
-        else:
-            # Create new user document
-            await user_collection.insert_one({
-                'id': receiver_id,
-                'username': gift['receiver_username'],
-                'first_name': gift['receiver_first_name'],
-                'characters': [gift['character']],
-            })
-
-        # Remove the gift offer from the pending gifts
-        del pending_gifts[(sender_id, receiver_id)]
-
-        await callback_query.message.edit_text(f"❖ ʏᴏᴜ ʜᴀᴠᴇ sᴜᴄᴄᴇssғᴜʟʟʏ ɢɪғᴛᴇᴅ ʏᴏᴜʀ ᴄʜᴀʀᴀᴄᴛᴇʀ ᴛᴏ ➥ [{gift['receiver_first_name']}](tg://user?id={receiver_id})")
-
-
+    await q.message.edit("🎁 ɢɪғᴛ sᴇɴᴛ")
